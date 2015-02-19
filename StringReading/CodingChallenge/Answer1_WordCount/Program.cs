c3c289b2c191fffb05dc18f599e1633169f8bdf7
Jamie Brindle <jamie.brindle7@gmail.com>
2015-02-19 21:59:17 +0000

    Another coding challenge added - StringReading , word count and reading in parallel
    For a pre-interview test

---
+++ StringReading/CodingChallenge/Answer1_WordCount/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using THGTests;
+
+namespace Answer1_WordCount
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintWordsAndCountInDecendingOrder(GenerateSortedWordLists(GenerateContent(new CharacterReader())));
+
+            Console.In.ReadLine();
+        }
+
+        /// <summary>
+        /// Reads characters from the CharacterReader class (GetNextChar() method) and outputs a single string.
+        /// </summary>
+        private static String GenerateContent(ICharacterReader reader)
+        {
+            var content = string.Empty;
+
+            while (true)
+            {
+                // the GetNextChar() method will inevitably break. I don't beleive that the method should be
+                // coded that way however it will need to be called within a try-catch block.
+
+                try
+                {
+                    content += reader.GetNextChar();
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            reader.Dispose();
+
+            return content;
+        }
+        
+        /// <summary>
+        /// Group words into lists within lists.
+        /// </summary>
+        public static List<List<String>> GenerateSortedWordLists(string content)
+        {
+            var wordLists = new List<List<string>>();
+            var words = new List<string>();
+
+            var punctuation = content.Where(Char.IsPunctuation).Distinct().ToArray(); // commas, colons, full stops etc.
+            var words_ienumerable = content.Split().Select(x => x.Trim(punctuation));
+
+            // convert into List so we can easily use the 'sort' method. Also remove empty strings.
+            foreach (var word in words_ienumerable)
+            {
+                if (word != string.Empty)
+                {
+                    words.Add(word);
+                }
+            }
+
+            // alphabetise
+            words.Sort();
+
+            var duplicates = words
+             .GroupBy(p => p)
+             .Where(g => g.Count() > 1)
+             .Select(g => g.Key);
+
+            foreach (var duplicate in duplicates)
+            {
+                var wordList = new List<string>();
+
+                foreach (var word in words)
+                {
+                    if (word == duplicate)
+                    {
+                        wordList.Add(word);
+                    }
+                }
+
+                wordLists.Add(wordList);
+            }
+
+            var singles = words
+             .GroupBy(p => p)
+             .Where(g => g.Count() == 1)
+             .Select(g => g.Key);
+
+            foreach (var word in singles)
+            {
+                wordLists.Add(new List<string> { word });
+            }
+
+            return wordLists;
+        }
+
+        public static void PrintWordsAndCountInDecendingOrder(List<List<String>> wordLists)
+        {
+            IEnumerable<List<string>> result = wordLists.OrderByDescending(x => x.Count);
+
+            foreach (var wordList in result)
+            {
+                try
+                {
+                    Console.Out.WriteLine(wordList[0] + " - " + wordList.Count());
+                }
+                catch (FormatException) 
+                { 
+                    // Do nothing
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    // Do nothing
+                }
+            }
+        }
+    }
+}
---
+++ StringReading/CodingChallenge/Answer2_Threading/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using THGTests;
+
+namespace Answer2_Threading
+{
+    /// <summary>
+    /// This class also utilises (re-uses) some methods created in the project 'Answer1_WordCount'
+    /// </summary>
+    class Program
+    {
+        private static long m_timerInterval = 10 * 1000; // 10 seconds
+        private static List<String> m_contentLists; // seperate list of different contents from different RandomCharacterReaders
+
+        private static volatile string m_allContent = string.Empty;        
+
+        private static RandomCharacterReader[] m_readers = 
+            {
+                new RandomCharacterReader(),
+                new RandomCharacterReader(),
+                new RandomCharacterReader(),
+                new RandomCharacterReader()
+            };
+
+        static void Main(string[] args)
+        {
+            m_contentLists = new List<string>();
+
+            new Timer(TimerTick, null, 0, m_timerInterval);
+
+            Console.In.ReadLine();
+        }
+
+        private static void TimerTick(Object input)
+        {
+            ProcessMultipleReadersInParallel();
+
+            m_allContent += String.Join(String.Empty, m_contentLists.ToArray());
+
+            // Decided to clear console and start afresh for the next timer tick.
+            Console.Clear();
+
+            var wordLists = Answer1_WordCount.Program.GenerateSortedWordLists(m_allContent);
+            Answer1_WordCount.Program.PrintWordsAndCountInDecendingOrder(wordLists);
+        }
+
+        private static void ProcessMultipleReadersInParallel()
+        {
+            var readerProcessThreads = new List<Thread>();
+
+            for (int i = 0; i < m_readers.Count(); i++)
+            {
+                // create position for the content for each RandomCharacterReader
+                m_contentLists.Add("");
+
+                readerProcessThreads.Add(new Thread(new ParameterizedThreadStart(GenerateContent)));
+                readerProcessThreads[i].IsBackground = true;
+
+                readerProcessThreads[i].Start(new object[] { m_readers[i], i });
+            }
+        }
+
+        /// <summary>
+        /// Reads characters from a RandomCharacterReader and inserts it into a specified content list.
+        /// </summary>
+        private static void GenerateContent(Object input)
+        {
+            object[] parameters = (object[])input;
+
+            var reader = (ICharacterReader)parameters[0];
+            var contentListIndex = (int)parameters[1];
+
+            var content = string.Empty;
+
+            while (true)
+            {
+                try
+                {
+                    m_contentLists[contentListIndex] += reader.GetNextChar();
+                }
+                catch
+                {
+                    // Do nothing
+                }
+            }
+        }
+    }
+}
